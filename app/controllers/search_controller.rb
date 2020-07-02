81ad84ead073c49ec7b6399563fbd95aff263274
Ethan Hocking <eshocking253@gmail.com>
2020-07-02 11:33:50 -0600

    Refactor search controller to use search results poro

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,5 @@
 class SearchController < ApplicationController
   def index
-    state = params[:state]
-
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_KEY"]
-    end
-
-    response = conn.get("/congress/v1/members/house/#{state}/current.json")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results].map do |member_data|
-      Member.new(member_data)
-    end
+    @members = SearchResults.new.members(params[:state])
   end
 end
